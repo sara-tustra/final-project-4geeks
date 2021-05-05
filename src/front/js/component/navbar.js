bff190d8a54da585bbd526c53cf52e20a24d6cd2
yerssin rios <yerssin15@gmail.com>
2021-05-05 14:57:47 -0400

    cambio nav

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,28 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
-      <Link to="/">
-        <span className="text-xl-left navbar-brand mb-0 h1">
-          Code/Gram que lo e
-        </span>
-      </Link>
-      <Link to="/">
-        <span className="text-right navbar-brand mb-0 h2">Inicio</span>
-      </Link>
-      <div className="ml-auto">
-        <Link to="/demo">
-          <button className="btn btn-danger mr-2">Registrarse</button>
-        </Link>
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
+			<Link to="/">
+				<span className="text-xl-left navbar-brand mb-0 h1">Code/Gram</span>
+			</Link>
+			<Link to="/">
+				<span className="text-right navbar-brand mb-0 h2">Inicio</span>
+			</Link>
+			<div className="ml-auto">
+				<Link to="/demo">
+					<button className="btn btn-danger mr-2">Registrarse</button>
+				</Link>
 
-        <Link to="/">
-          <span className="navbar-brand mb-0 h2">Preguntas Frecuentes</span>
-        </Link>
-        <Link to="/">
-          <span className="navbar-brand mb-0 h2">Login</span>
-        </Link>
-      </div>
-    </nav>
-  );
+				<Link to="/">
+					<span className="navbar-brand mb-0 h2">Preguntas Frecuentes</span>
+				</Link>
+				<Link to="/">
+					<span className="navbar-brand mb-0 h2">Login</span>
+				</Link>
+			</div>
+		</nav>
+	);
 };
