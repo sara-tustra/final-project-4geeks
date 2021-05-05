e77fe6061eaa48a0d95d5ac55c96fa4194302685
Henry Romero <hromerp22@gmail.com>
2021-05-05 21:57:06 +0200

    cambio4

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,19 +7,18 @@ export const Navbar = () => {
 			<Link to="/">
 				<span className="text-xl-left navbar-brand mb-0 h1">Code/Gram</span>
 			</Link>
-			<Link to="/">
-				<span className="text-right navbar-brand mb-0 h2">Inicio</span>
-			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
 					<button className="btn btn-danger mr-2">Registrarse</button>
 				</Link>
-
+				<Link to="/">
+					<span className="navbar-brand mb-0 h2">Login</span>
+				</Link>
 				<Link to="/">
 					<span className="navbar-brand mb-0 h2">Preguntas Frecuentes</span>
 				</Link>
 				<Link to="/">
-					<span className="navbar-brand mb-0 h2">Login</span>
+					<span className="text-right navbar-brand mb-0 h2">Inicio</span>
 				</Link>
 			</div>
 		</nav>
