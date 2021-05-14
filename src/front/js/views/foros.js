3135d625a540f6e52c0bc165ddf877f05a9e68b1
Henry Romero <hromerp22@gmail.com>
2021-05-14 17:30:52 +0200

    forooo

--- src/front/js/views/foros.js
+++ src/front/js/views/foros.js
@@ -7,10 +7,14 @@ export const Foros = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Foro</h1>
+			<div className="h12">
+				<h1 contentEditable spellCheck="false">
+					Foro
+				</h1>
+			</div>
 			<div className="container mt-5 mb-5">
 				<div className="row d-flex align-items-center justify-content-center">
-					<div className="col-md-6">
+					<div className="col-md-10">
 						<div className="card">
 							<div className="d-flex justify-content-between p-2 px-3">
 								<div className="d-flex flex-row align-items-center">
@@ -19,7 +23,18 @@ export const Foros = () => {
 									<div className="d-flex flex-column ml-2">
 										{" "}
 										<span className="font-weight-bold">Carla Rodriguez</span>{" "}
-										<small className="text-primary">Collegues</small>{" "}
+										<div className="comment-input">
+											{" "}
+											<input
+												type="text"
+												className="form-control"
+												placeholder="Agrega un titulo"
+											/>
+											<div className="fonts">
+												{" "}
+												<i className="usuario fa fa-user" />{" "}
+											</div>
+										</div>
 									</div>
 								</div>
 								<div className="d-flex flex-row mt-1 ellipsis">
@@ -28,20 +43,39 @@ export const Foros = () => {
 								</div>
 							</div>{" "}
 							<div className="p-2">
-								<p className="text-justify">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									incididunt.
-								</p>
+								<div className="share border bg-white">
+									<div className="d-flex flex-row inputs p-2 py-4">
+										<img
+											className="rounded-circle"
+											src="https://i.imgur.com/UXdKE3o.jpg"
+											width={50}
+										/>
+										<span className="font-weight-bold">Carla Rodriguez</span>{" "}
+										<input
+											type="text"
+											className="border-0 form-control share-input"
+											placeholder="Agrega un contenido"
+										/>
+									</div>
+									<div className="d-flex flex-row justify-content-between border-top">
+										<div className="d-flex flex-row publish-options" />
+										<div className="d-flex flex-row icons d-flex align-items-center">
+											{" "}
+											<i className="fa fa-heart" />
+											<div className="d-flex flex-row align-items-center status">
+												{" "}
+												<small>Like</small> <small>Comentar</small>{" "}
+											</div>{" "}
+										</div>
+										<div className="publish-button">
+											<div className="align-items-center border-left p-2 px-5 btn publish">
+												<span className="ml-1">Publish</span>
+											</div>
+										</div>
+									</div>
+								</div>
 								<hr />
 								<div className="d-flex justify-content-between align-items-center">
-									<div className="d-flex flex-row icons d-flex align-items-center">
-										{" "}
-										<i className="fa fa-heart" />
-										<div className="d-flex flex-row align-items-center status">
-											{" "}
-											<small>Like</small> <small>Comentar</small>{" "}
-										</div>{" "}
-									</div>
 									<div className="d-flex flex-row muted-color">
 										{" "}
 										<span>1 comments</span>{" "}
@@ -64,7 +98,11 @@ export const Foros = () => {
 									</div>
 									<div className="comment-input">
 										{" "}
-										<input type="text" className="form-control" />
+										<input
+											type="text"
+											className="form-control"
+											placeholder="Agrega un comentario"
+										/>
 										<div className="fonts">
 											{" "}
 											<i className="usuario fa fa-user" />{" "}
