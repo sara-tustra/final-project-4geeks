1e33dcefea3489ad31712cd364a58bc729a395bc
Henry Romero <hromerp22@gmail.com>
2021-05-16 00:39:54 +0200

    elimineotones

--- src/front/js/component/botonlike.js
+++ src/front/js/component/botonlike.js
@@ -21,3 +21,36 @@ function Botonlike() {
 }
 
 export default Botonlike;
+
+/* import React, { Component, useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/botones.scss";
+
+export class Botones extends Component {
+	constructor(props) {
+		super(props);
+
+		this.incrementar = this.incrementar.bind(this);
+
+		this.state = {
+			contador: 0
+		};
+	}
+	incrementar() {
+		console.log("EL usuario le dio click al boton", this);
+		this.setState({
+			contador: this.state.contador + 1
+		});
+	}
+	render() {
+		return (
+			<div className="container">
+				<button onClick={this.incrementar} className="txpro">
+					<i className="fas fa-thumbs-up" />
+				</button>
+				<div>{this.state.contador}</div>
+			</div>
+		);
+	}
+}
+ */
