b7f8819004853380f713fedb130fce02f36a431b
yerssin rios <yerssin15@gmail.com>
2021-05-19 20:03:23 -0400

    ipa tercero listo

--- src/front/js/component/WeatherInfo.js
+++ src/front/js/component/WeatherInfo.js
@@ -1,54 +1,90 @@
-import React from "react";
-import PropTypes from "prop-types";
-const WeatherInfo = props => {
-	console.log(props);
-	return (
-		<div>
-			{props.error && (
-				<div className="alert alert-danger">
-					<p>{props.error}</p>
-				</div>
-			)}
-			{props.temperature ? (
-				<div className="card card-body mt-2 animated fadeInUp">
-					{props.city &&
-						props.country && (
+import React, { Component } from "react";
+import "../../styles/WeatherInfo.scss";
+import { WEATHER_KEY } from "../keys";
+import { Circle } from "../component/Circle";
+
+class WeatherInfo extends Component {
+	state = {
+		temperature: "",
+		description: "",
+		humidity: "",
+		wind_speed: 0,
+		city: "santiago",
+		country: "chile",
+		error: null
+	};
+
+	getWeather = async () => {
+		const { city, country } = this.state;
+		const cityValue = city;
+		const countryValue = country;
+
+		if (cityValue && countryValue) {
+			// metric parameter is for Celcius Unit
+			const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue}, ${countryValue}&appid=${WEATHER_KEY}`;
+			const response = await fetch(API_URL);
+			const data = await response.json();
+			console.log(data);
+
+			this.setState({
+				temperature: data.main.temp,
+				description: data.weather[0].description,
+				humidity: data.main.humidity,
+				wind_speed: data.wind.speed,
+				city: data.name,
+				country: data.sys.country,
+				error: null
+			});
+		} else {
+			this.setState({
+				error: "Please enter a City and a Country."
+			});
+		}
+	};
+
+	componentDidMount() {
+		this.getWeather();
+	}
+
+	render() {
+		return (
+			<>
+				{this.state.temperature ? (
+					<div className="card-1 card-body mt-2 animated fadeInUp mx-auto">
+						<Circle />
+						<p>Temperature</p>
+						{this.state.city &&
+							this.state.country && (
+								<p>
+									<i className="fas fa-location-arrow" /> Location: {this.state.city},{" "}
+									{this.state.country}
+								</p>
+							)}
+						{this.state.temperature && (
 							<p>
-								<i className="fas fa-location-arrow" /> Location: {props.city}, {props.country}
+								<i className="fas fa-temperature-low" /> Temperature: {this.state.temperature} ℃,{" "}
+								{this.state.description}
 							</p>
 						)}
-					{props.temperature && (
-						<p>
-							<i className="fas fa-temperature-low" /> Temperature: {props.temperature} ℃,{" "}
-							{props.description}
-						</p>
-					)}
-					{props.humidity && (
-						<p>
-							<i className="fas fa-water" /> Humidity: {props.humidity}
-						</p>
-					)}
-					{props.wind_speed && (
-						<p>
-							<i className="fas fa-wind" /> Wind Speed: {props.wind_speed}
-						</p>
-					)}
-				</div>
-			) : (
-				<div className="card card-body mt-2 text-center">
-					<i className="fas fa-sun fa-10x" />
-				</div>
-			)}
-		</div>
-	);
-};
-WeatherInfo.propTypes = {
-	error: PropTypes.string,
-	temperature: PropTypes.string,
-	city: PropTypes.string,
-	country: PropTypes.string,
-	description: PropTypes.string,
-	humidity: PropTypes.string,
-	wind_speed: PropTypes.string
-};
+						{this.state.humidity && (
+							<p>
+								<i className="fas fa-water" /> Humidity: {this.state.humidity}
+							</p>
+						)}
+						{this.state.wind_speed && (
+							<p>
+								<i className="fas fa-wind" /> Wind Speed: {this.state.wind_speed}
+							</p>
+						)}
+					</div>
+				) : (
+					<div className="card card-body  text-center">
+						<i className="fas fa-sun fa-10x" />
+					</div>
+				)}
+			</>
+		);
+	}
+}
+
 export default WeatherInfo;
